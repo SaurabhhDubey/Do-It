6017031bba18b1c78de171205d70f1868650e897
Saurabh Dubey <saurabhdubey3712@gmail.com>
2026-01-24 17:21:14 +0530

    login auth api

--- frontend/src/api/authAPI.js
+++ frontend/src/api/authAPI.js
@@ -18,4 +18,12 @@ export const registerUser = async(username,email,password)=>{
     const data = await response.json();
     return data.user;
 
-}
+};
+
+
+
+ export const loginUser = async(email , password)=>{
+    const response = await fetch(`${BASE_URL}/api/users/login` , {method:"post" , headers:{"Content-Type":"application/json"},body:JSON.stringify({email , password}) , });
+    const data = await  response.json();
+    return data;
+ };
